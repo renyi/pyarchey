508898423a453def00984741b06f0291748d2f5b
walchko <kevin.walchko@outlook.com>
2015-09-25 10:09:35 -0600

    fix tests

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import sys
-sys.path.insert(0, '../')
+# import sys
+# sys.path.insert(0, '../')
 
 import pyarchey.pyarchey as py
 
@@ -13,12 +13,12 @@ import pyarchey.pyarchey as py
 
 def test_slack():
 	o = py.Output()
-	assert o.readDistro('./slack.test') == ('Slackware', 'Slackware 14.1')
+	assert o.readDistro('./test/slack.test') == ('Slackware', 'Slackware 14.1')
 	
 def test_arch():
 	o = py.Output()
-	assert o.readDistro('./arch.test') == ('Arch Linux', 'Arch Linux')
+	assert o.readDistro('./test/arch.test') == ('Arch Linux', 'Arch Linux')
 
 def test_raspbian():
 	o = py.Output()
-	assert o.readDistro('./raspbian.test') == ('Raspbian', 'Raspbian 7 (wheezy)')
+	assert o.readDistro('./test/raspbian.test') == ('Raspbian', 'Raspbian 7 (wheezy)')
